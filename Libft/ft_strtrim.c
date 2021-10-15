429dcbb2d687eb11e7772d6901d683a4e4982c05
Yacine Bentayeb <ybentaye@c1r14s2.42lausanne.ch>
2021-10-15 12:56:01 +0200

    adding strtrim

--- Libft/ft_strtrim.c
+++ Libft/ft_strtrim.c
@@ -1,67 +1,62 @@
 #include "libft.h"
 
-int	ft_strind(const char *haystack, const char *needle)
+
+int	search_in(char c, char const *set)
 {
 	int	i;
-	int	j;
-	int k;
 
 	i = 0;
-	j = 0;
-	k = 0;
-	if (needle[0] == 0)
-		return (-2);
-	while (haystack[i])
+	while (set[i])
 	{
-		j = 0;
-		while (haystack[i + j] == needle[j])
+		if (set[i] == (char)c)
 		{
-			if (needle[j + 1] == 0)
-				return (i);
-			j++;
-			k++;
+			return (1);
 		}
 		i++;
-		k++;
 	}
-	return (-1);
+	return (0);
 }
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*str;
 	int 	len_set;
-	int 	len_s1;
-    int     i;
+	int		i;
+	int     start;
+	int		end;
 
-    len_s1 = ft_strlen((char *)s1);
-    len_set = ft_strlen((char *)set);
-    str = (char *)malloc(sizeof(char)* (len_s1 + 1));
-    if (!str)
-    {
-        return (0);
-    }
-    i = 0;
-    if (ft_strind(s1, set) == 0)
-    {
-        s1 += len_set;
-    }
-    len_s1 = ft_strlen((char *)s1);
-    if (ft_strind(s1, set) != len_s1 - len_set)
-        len_set = 0;
-    while (s1[i] && i < len_s1 - len_set)
-    {
-        str[i] = s1[i];
-        i++;
-    }
-    str[i] = '\0';
-    return(str);
+	len_set = ft_strlen((char *)set);
+	start = 0;
+	end = ft_strlen((char *)s1);
+	i = 0;
+	if (set == 0 || s1 == 0)
+		return 0;
+	while(search_in((char)s1[start], set))
+	{
+		start++;
+	}
+	while(search_in((char)s1[end - 1], set))
+	{
+		end--;
+	}
+	str = (char *)malloc(sizeof(char) * (end - start + 1));
+	if (!str)
+		return (0);
+	while(start < end)
+	{
+		str[i] = s1[start];
+		i++;
+		start++;
+	}
+	str[i] = 0;
+	return (str);
 }
 
 int main()
 {
-	char *s1 = "----my favorite animal is  ";
-	char *s2 = "--";
+	char *s1 = "";
+	char *s2 = "";
 	printf("%s",ft_strtrim(s1,s2));
-		return (0);
+	return (0);
 }
+
