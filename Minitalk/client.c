a0a4005d9848893ce069d85947f5000e1e9d7af5
Yacine Bentayeb <yacinbent@gmail.com>
2021-12-05 11:45:05 +0100

    update minitalk

--- Minitalk/client.c
+++ Minitalk/client.c
@@ -3,21 +3,48 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ybentaye <ybentaye@student.42lausanne.c    +#+  +:+       +#+        */
+/*   By: yacinebentayeb <yacinebentayeb@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 11:52:43 by ybentaye          #+#    #+#             */
-/*   Updated: 2021/12/02 12:29:21 by ybentaye         ###   ########.fr       */
+/*   Updated: 2021/12/03 13:48:32 by yacinebenta      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+int send_str(char *str, int sign, int len)
+{
+	int	i;
+	int	j;
+
+	j = 0;
+	while (j < len)
+	{
+		while (i < 7)
+		{
+			i = 0;
+			if ((str[j] >> i) & 1)
+				kill(9316, SIGUSR1);
+			else
+				kill(9316, SIGUSR2);
+			i++;
+			sleep(0.1);
+		}
+		j++;
+	}
+}
+
 // check SIGUSR1 SIGUSR2
-int	main()
+int	main(int argc, char **argv)
 {
 	int sig;
+	int	sign;
+	int	len;
 
+	sign = ft_atoi(argv[1]);
+	len = ft_strlen(argv[2]);
 	sig = SIGUSR1;
 	ft_printf("le signal: %d\n", sig);
+	kill(9316, SIGUSR1);
 	return (0);
 }
--- Minitalk/server.c
+++ Minitalk/server.c
@@ -3,26 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ybentaye <ybentaye@student.42lausanne.c    +#+  +:+       +#+        */
+/*   By: yacinebentayeb <yacinebentayeb@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 11:52:48 by ybentaye          #+#    #+#             */
-/*   Updated: 2021/12/02 12:25:16 by ybentaye         ###   ########.fr       */
+/*   Updated: 2021/12/03 10:30:41 by yacinebenta      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-
+void	get_signal(int signum);
 //check bitshift
-int	main(int argc, char **argv)
+int	main(void)
 {
 	int	i;
 
 	i = 0;
-	ft_putnbr_fd(argc, 1);
-	ft_putstr_fd(argv[1], 1);
+
 	ft_printf("l'id du server: %d", getpid());
+	signal(SIGUSR1, get_signal);
 	while (1)
 		sleep(1);
 	return (0);
+}
+
+void	get_signal(int signum)
+{
+	ft_printf("got the signal: %d\n", signum);
+	exit (0);
 }
